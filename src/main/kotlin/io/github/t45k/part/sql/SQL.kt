5b95b48fcb8fedbcae8eef9d52520c3b683eb7e1
T45K <tasktas9@gmail.com>
2020-02-20 14:48:26 +0900

    impl

--- src/main/kotlin/io/github/t45k/part/sql/SQL.kt
+++ src/main/kotlin/io/github/t45k/part/sql/SQL.kt
@@ -1,5 +1,6 @@
 package io.github.t45k.part.sql
 
+import io.github.t45k.part.entity.RawMethodHistory
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.PreparedStatement
@@ -9,58 +10,52 @@ import java.sql.Statement
 class SQL {
     private val connection: Connection = DriverManager.getConnection("jdbc:sqlite:./db.sqlite3")
             ?: throw RuntimeException("Bad db connection")
-    private val paramInsertionStatement: PreparedStatement = connection.prepareStatement(paramInsertionQuery)
     private val methodInsertionStatement: PreparedStatement = connection.prepareStatement(methodInsertionQuery)
+    private val revisionInsertionStatement: PreparedStatement = connection.prepareStatement(revisionInsertionQuery)
+    private val contentsInsertionStatement: PreparedStatement = connection.prepareStatement(contentsInsertionQuery)
 
     companion object {
-        const val paramInsertionQuery: String = "insert into param values(?, ?, ?)"
-        const val methodInsertionQuery: String = "insert into method(name, isConstructor, numOfParams) values(?, ?, ?)"
+        const val methodInsertionQuery: String = "insert into method values(?)"
+        const val revisionInsertionQuery: String = "insert into revision(fileName, hash, message) values(?, ?, ?)"
+        const val contentsInsertionQuery: String = "insert into contents values(?, ?)"
     }
 
     init {
         val statement: Statement = connection.createStatement()
-        statement.executeUpdate("create table if not exists method (name string, isConstructor integer, numOfParams integer, id integer primary key autoincrement)")
-        statement.executeUpdate("create table if not exists param (id integer, declaredOrder integer, referredOrder integer)")
+        statement.executeUpdate("create table if not exists method (fileName string primary key)")
+        statement.executeUpdate("create table if not exists revision (fileName string, hash string, message string, id integer primary key autoincrement)")
+        statement.executeUpdate("create table if not exists contents (body string, id integer)")
+        statement.close()
     }
 
-    fun insert(method: Method) {
-        val id: Int = insertMethod(method)
-        if (method.numOfParams <= 1) {
-            return
-        }
-
-        for ((declaredOrder, referredOrder) in method.order.withIndex()) {
-            insertParam(id, declaredOrder, referredOrder)
-        }
-    }
-
-    private fun insertParam(id: Int, declaredOrder: Int, referredOrder: Int) {
-        val paramInsertionStatement: PreparedStatement = connection.prepareStatement(paramInsertionQuery)
-        paramInsertionStatement.setInt(1, id)
-        paramInsertionStatement.setInt(2, declaredOrder)
-        paramInsertionStatement.setInt(3, referredOrder)
-        paramInsertionStatement.executeUpdate()
-    }
-
-    private fun insertMethod(method: Method): Int {
-        val name = "${method.path}#${method.name}"
-        val isConstructor: Int = if (method.isConstructor) 1 else 0
-        methodInsertionStatement.setString(1, name)
-        methodInsertionStatement.setInt(2, isConstructor)
-        methodInsertionStatement.setInt(3, method.numOfParams)
+    fun insert(methodHistory: RawMethodHistory) {
+        val fileName: String = methodHistory.fileName
+        methodInsertionStatement.setString(1, fileName)
         methodInsertionStatement.executeUpdate()
 
-        val generatedKey: ResultSet = methodInsertionStatement.generatedKeys
-        generatedKey.next()
-        val id: Int = generatedKey.getInt(1)
-        generatedKey.close()
+        for (revision in methodHistory.rawRevisions) {
+            val (commitHash, commitMessage, rawBody) = revision
+            revisionInsertionStatement.setString(1, fileName)
+            revisionInsertionStatement.setString(2, commitHash)
+            revisionInsertionStatement.setString(3, commitMessage)
+            revisionInsertionStatement.executeUpdate()
 
-        return id
+            val generatedKey: ResultSet = revisionInsertionStatement.generatedKeys
+            generatedKey.next()
+            val id: Int = generatedKey.getInt(1)
+            generatedKey.close()
+
+            contentsInsertionStatement.setString(1, rawBody)
+            contentsInsertionStatement.setInt(2, id)
+            contentsInsertionStatement.executeUpdate()
+        }
     }
 
     fun close() {
-        paramInsertionStatement.close()
         methodInsertionStatement.close()
+        revisionInsertionStatement.close()
+        contentsInsertionStatement.close()
+        methodInsertionStatement.clearParameters()
         connection.close()
     }
 }
