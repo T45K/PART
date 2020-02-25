31dc06ef20de24968a79e339a45a43fc8f3ccea1
T45K <tasktas9@gmail.com>
2020-02-25 11:24:51 +0900

    impl sql

---
+++ src/main/kotlin/io/github/t45k/part/sql/Schema.kt
@@ -0,0 +1,38 @@
+package io.github.t45k.part.sql
+
+class FileNameSchema {
+    companion object {
+        override fun toString() = "$TABLE_NAME (${Column.FILE_NAME} string primary key)"
+        private const val TABLE_NAME = "file_names"
+        const val INSERTION_QUERY = "insert into $TABLE_NAME values(?)"
+        const val ALL_SELECTION_QUERY = "select * from method"
+    }
+}
+
+class RevisionSchema {
+    companion object {
+        override fun toString() = "$TABLE_NAME (${Column.FILE_NAME} string, ${Column.COMMIT_HASH} string, ${Column.COMMIT_MESSAGE} string, ${Column.ID} integer primary key autoincrement"
+        private const val TABLE_NAME = "revisions"
+        const val INSERTION_QUERY = "insert into $TABLE_NAME (${Column.FILE_NAME}, ${Column.COMMIT_HASH}, ${Column.COMMIT_MESSAGE}) values(?, ?, ?)"
+        const val SELECTION_QUERY = "select * from $TABLE_NAME where ${Column.FILE_NAME} = ?"
+    }
+}
+
+class ContentsSchema {
+    companion object {
+        override fun toString(): String = "contents (${Column.CONTENT} string, ${Column.ID} integer primary key"
+        private const val TABLE_NAME = "contents"
+        const val INSERTION_QUERY = "insert into $TABLE_NAME values(?, ?)"
+        const val SELECTION_QUERY = "select * from $TABLE_NAME where ${Column.ID}=?"
+    }
+}
+
+class Column {
+    companion object {
+        const val FILE_NAME = "file_name"
+        const val COMMIT_HASH = "commit_hash"
+        const val COMMIT_MESSAGE = "commit_message"
+        const val ID = "id"
+        const val CONTENT = "content"
+    }
+}
