00ac8e24f818277f35c7afde10ddca9beccd84d7
T45K <tasktas9@gmail.com>
2020-03-04 11:21:37 +0900

    subtle modify

--- src/main/kotlin/io/github/t45k/part/mining/git/GitCommand.kt
+++ src/main/kotlin/io/github/t45k/part/mining/git/GitCommand.kt
@@ -2,6 +2,6 @@ package io.github.t45k.part.mining.git
 
 import java.nio.file.Path
 
-abstract class GitCommand<I, O>(protected val projectRootPath: Path, protected val filePath: Path) {
-    abstract fun execute(input: I): O
+abstract class GitCommand<Input, Output>(protected val projectRootPath: Path, protected val filePath: Path) {
+    abstract fun execute(input: Input): Output
 }
