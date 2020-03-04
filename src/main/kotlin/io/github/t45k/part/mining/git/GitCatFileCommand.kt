a6eba0f3be1d9ff05b2c243fa4ccc7ff1df39451
T45K <tasktas9@gmail.com>
2020-03-04 11:12:43 +0900

    impl

--- src/main/kotlin/io/github/t45k/part/mining/git/GitCatFileCommand.kt
+++ src/main/kotlin/io/github/t45k/part/mining/git/GitCatFileCommand.kt
@@ -1,18 +1,23 @@
 package io.github.t45k.part.mining.git
 
 import com.github.kusumotolab.sdl4j.util.CommandLine
-import java.nio.file.Files
 import java.nio.file.Path
 
 class GitCatFileCommand(projectRootPath: Path, filePath: Path) : GitCommand<String, String>(projectRootPath, filePath) {
-    private val gitCheckOutCommand: Array<String> = arrayOf("git", "checkout")
+    private val gitCatFileCommand: Array<String> = arrayOf("git", "cat-file", "-p")
 
+    /**
+     * execute "git cat-file -p <hash>:<path>" command
+     *
+     * @param input target commit hash
+     * @return file contents on target commit
+     */
     override fun execute(input: String): String {
-        val result: CommandLine.CommandLineResult = CommandLine().forceExecute(projectRootPath.toFile(), *gitCheckOutCommand, input, filePath.toString())
+        val result: CommandLine.CommandLineResult = CommandLine().forceExecute(projectRootPath.toFile(), *gitCatFileCommand, "$input:$filePath")
                 ?: return ""
 
         return if (result.isSuccess) {
-            String(Files.readAllBytes(filePath))
+            result.outputLines.joinToString(" ")
         } else {
             ""
         }
